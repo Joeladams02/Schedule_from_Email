135faf8eda1e8f541cfd3e5ac417ecaebcff1135
Joel Adams <Joeladams02@icloud.com>
2025-01-11 12:14:01 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from Handle_Emails import EmailHandler
-
-email_handler = EmailHandler(username="Joeladams02@icloud.com", 
-                             password="vttg-qutv-cvxe-tkon", 
-                             server="imap.mail.me.com")
-
-email_handler.send_email('Schedule.ics')
